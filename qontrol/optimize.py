89d83b65de3c0d57341910deb64e615d3fcc224b
HarshBabla99 <harshbabla@gmail.com>
2026-08-16 21:52:39 -0400

    fix how costs are unpacked in _calculate_rel_cost_diff

--- qontrol/optimize.py
+++ qontrol/optimize.py
@@ -401,7 +401,7 @@ def _calculate_total_norm(values: Array | dict) -> np.ndarray | float:
 
 def _calculate_rel_cost_diff(opt_recorder: OptimizerRecorder) -> bool:
     """Check if cost change is below tolerance."""
-    current_total_cost, prev_total_cost = opt_recorder.total_costs[-2:]
+    prev_total_cost, current_total_cost = opt_recorder.total_costs[-2:]
     cost_diff = current_total_cost - prev_total_cost
     return np.max(np.abs(cost_diff / current_total_cost))
 
